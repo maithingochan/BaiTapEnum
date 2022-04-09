ac0b4c032f16c6d4e956104720c2ea6aeed98c1e
THANHHA <hadoansssc@gmail.com>
2022-04-09 22:46:11 +0700

    add file

--- ConsoleApp8/ConsoleApp8/Program.cs
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -4,12 +4,10 @@ namespace ConsoleApp8
 {
     class Program
     {
-        enum HocLuc
+        enum GioiTinh
         {
-            Tốt,
-            Khá,
-            Trung_Bình,
-            Yếu
+            Nam =0,
+            Nu =1
         }
         struct SinhVien
         {
@@ -18,15 +16,16 @@ namespace ConsoleApp8
             public double DiemToan;
             public double DiemLy;
             public double DiemHoa;
+            public int GioiTinh;
 
-            public  SinhVien(String msv, string name, double t, double l, double h)
+            public  SinhVien(String msv106, string name106, double t106, double l106, double h106,int gt106)
             {
-                MaSo = msv;
-                HoTen = name;
-                DiemToan = t;
-                DiemLy = l;
-                DiemHoa = h;
-                
+                MaSo = msv106;
+                HoTen = name106;
+                DiemToan = t106;
+                DiemLy = l106;
+                DiemHoa = h106;
+                GioiTinh = gt106;
             }
         }
 
@@ -36,6 +35,8 @@ namespace ConsoleApp8
             x.MaSo = Console.ReadLine();
             Console.WriteLine("Nhập Họ và Tên:");
             x.HoTen = Console.ReadLine();
+            Console.WriteLine("Giới tính(nữ  0,nam 1, khác) :");
+            x.GioiTinh = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Nhập Điểm toán:");
             x.DiemToan = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Nhập Điểm lý:");
@@ -45,11 +46,26 @@ namespace ConsoleApp8
 
         }
 
+        static string  ktgioitinh(SinhVien x)
+        {
+            //GioiTinh gt = GioiTinh.Nam;
+            if ((int)GioiTinh.Nam == 0)
+            {
+                return "Nam";
+            }
+            else if ((int)GioiTinh.Nu == 1)
+                return "Nữ";
+            else
+                return "Khác";
+
+        }
+
         static void xuatSV(SinhVien x)
         {
             Console.WriteLine("*****************");
             Console.WriteLine("Ma SV: {0}", x.MaSo);
             Console.WriteLine("Ho và tên : {0}", x.HoTen);
+            Console.WriteLine("Giới Tính: {0}", ktgioitinh(x));
             Console.WriteLine("Điểm toán: {0}", x.DiemToan);
             Console.WriteLine("Điểm lý: {0}", x.DiemLy);
             Console.WriteLine("Điểm hóa: {0}", x.DiemHoa);
@@ -68,10 +84,9 @@ namespace ConsoleApp8
         {
             for (int i = 0; i < n; i++)
             {
-                // cách 2 nhập trực tiếp vào vùng nhớ cửa mảng 
-                Console.WriteLine("***************************");
+                // cách 2 nhập trực tiếp vào vùng nhớ cửa mảng
                 xuatSV(arr[i]);
-
+                
             }
         }
         static double DTB(SinhVien x)
@@ -79,6 +94,21 @@ namespace ConsoleApp8
             double DTB = (x.DiemToan + x.DiemLy + x.DiemHoa) / 3;
             DTB = Math.Round(DTB, 2, MidpointRounding.AwayFromZero);
             return DTB;
+        }
+        static void XapxepHoTenSV(SinhVien[] arr, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (String.Compare(arr[i].HoTen, arr[j].HoTen) > 0)
+                    {
+                        SinhVien t = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = t;
+                    }
+                }
+            }
         }
         static void Main(string[] args)
         {
@@ -91,6 +121,7 @@ namespace ConsoleApp8
             //double l;
             //double h;
             NhapMangSV(arr,n);
+            XapxepHoTenSV(arr, n);
             XuatMangSV(arr, n);
         }
     }
